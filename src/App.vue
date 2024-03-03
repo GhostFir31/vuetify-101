4b188b8707a0de89a82e5ece05d657931172ea69
GhostFir31 <j3sus0marle@gmail.com>
2024-03-03 14:49:55 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon> </v-app-bar-nav-icon>
+      </template>
+
+    <v-app-bar-title>Menu</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer>
+      <v-list-item link to="/" title="INICIO"></v-list-item>
+      <v-list-item link to="/imagen" title="JOKES Y IMAGENES"></v-list-item>
+      <v-list-item link to="/tabla" title="DATOS"></v-list-item>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      Main Content
+    </v-main>
+  </v-layout>
+</template>
